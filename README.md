3e97d6f78c204822bb7264b318d721598af46543
Bruno Alves <bruno.s.alves@tecnico.ulisboa.pt>
2022-01-30 23:02:22 +0000

    Refactoring code
    - add dht11 to the structure of the code
    - refactor the structure of the code
    - it compiles but running on the board is required

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # SensoringTheRoom
 
 This is a personal project to measure the Temperature and Humidity of a room.
+
+* collect temperature from a LN35 sensor
+* collect temperature and humidity from a DHT11
+* stream that data to the computer via serial
+* produce visualization of the data
+
---
+++ streamTemperature/streamTemperature.ino
@@ -0,0 +1,72 @@
+
+/* Credits to Bruno Alves, created 09/11/2021*/
+
+/* LN35 SENSOR:
+   Arduino Uno contains a multichannel, 10-bit analog to digital converter. 
+   This means that it will map input voltages between 0 and the operating 
+   voltage 5V into integer values between 0 and 1023. However, the sensor
+   only produces voltages from 0 to +1V. The ADC uses 5V as the highest
+   possible value. This is wasting 80% of the possible range. 
+   If you change aRef to 1.1V, you will get almost the highest resolution
+   possible.
+
+   Using 1.1V as aRef, the equation changes. If you divide 1.1V over 1024, 
+   each step up in the analog reading is equal to approximately 1.0742 mV. 
+   If 10mV is equal to 1 degree Celcius, 10 / 1.0742 = ~9.31. So, for every 
+   change of 9.31 in the analog reading, there is one degree of temperature 
+   change.
+ */
+
+// LN35 Sensor
+constexpr float BASE_CELSIUS = 9.31;
+constexpr short int measureEvery = 5*60*1000;
+constexpr byte numberOfSamplesAverage = 3;
+
+byte analogPin = 1;
+
+// Data
+float temp35 = 0.0;
+float temp11 = 0.0;
+float humid = 0.0;
+
+void setup() {
+  Serial.begin(9600);
+
+  // Setup for LN35 sensor
+  //analogReference(INTERNAL); // change aRef to 1.1V
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {  
+  measureFromLn35InCelsius(&temp35);
+  measureFromDht11(&temp11, &humid);
+  activateLedNotification();
+  displayDataOnSerial();
+}
+
+void  measureFromLn35InCelsius(float* result) {
+  (*result) = 0.0;
+  for (byte i=0; i<numberOfSamplesAverage; i++) {
+    (*result) += analogRead(analogPin)/BASE_CELSIUS;
+  }
+  (*result) = (*result)/numberOfSamplesAverage;
+}
+
+void measureFromDht11(float* temp, float* hum) {
+}
+
+void activateLedNotification() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(250);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void displayDataOnSerial() {
+  Serial.print(temp35);
+  Serial.print(", ");
+  Serial.print(temp11);
+  Serial.print(", ");
+  Serial.print(humid);
+  Serial.println();
+}
