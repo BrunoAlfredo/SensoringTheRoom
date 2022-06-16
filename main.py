3ab96e48c436c6900d87698fb488d77aa41f47cf
Bruno Alves <bruno.s.alves@tecnico.ulisboa.pt>
2022-06-16 13:08:48 +0100

    Add user interface to main

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-import Worker
+
+import sys
+#import Worker
 
 def main():
     # perform what I want to do
     return 0
 
+def processDataFromCsv(filename):
+    print("Processing data from Csv... --> " + filename)
+    return 0
+
+def collectDataFromSerial():
+    print("Collecting data from Serial ...")
+    return 0
+
 if __name__ == "__main__":
-    main()
+    if (len(sys.argv) < 2):
+        print("\n*Lack of parameters. You can use the following options:")
+        print("\n   processCsv\n   readSerial")
+    elif (sys.argv[1] == "processCsv"):
+        if (len(sys.argv) == 2):
+            print("\n*Csv file is missing, please append the name after processCsv option")
+        else:
+            processDataFromCsv(sys.argv[2])
+    elif (sys.argv[1] == "readSerial"):
+        collectDataFromSerial()
+    else:
+        print("\n*Option not valid, please use these ones:")
+        print("\n   processCsv\n   readSerial")
+
