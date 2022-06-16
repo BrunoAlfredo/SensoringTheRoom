5cdf5a9f6829738d151b9f1a7a365d57fef8298d
Bruno Alves <bruno.s.alves@tecnico.ulisboa.pt>
2022-06-16 12:28:08 +0100

    Add OOP paradigm
    - more flexibility to select which operations to perform

---
+++ Worker.py
@@ -0,0 +1,19 @@
+
+class Worker:
+    """Utility class"""
+    parameters
+
+    def __init__(self, ...construtor arguments):
+        self.a = a
+        assigned to attributes
+
+    def fromSerialToCsv(self):
+        """Collect real time data"""
+        return 0
+    def loadDataFromCsvToDataStructure():
+        """Post processing"""
+        return 0
+    def buildVisuals():
+        """Post processing"""
+        return 0
+
---
+++ main.py
@@ -0,0 +1,8 @@
+import Worker
+
+def main():
+    # perform what I want to do
+    return 0
+
+if __name__ == "__main__":
+    main()
